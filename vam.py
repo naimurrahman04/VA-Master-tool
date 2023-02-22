7eb6af1240f219245d3e109a86e7aba03692d584
Naimur Rahman <59091676+naimurrahman04@users.noreply.github.com>
2023-02-22 14:57:35 +0600

    Update vam.py

--- vam.py
+++ vam.py
@@ -10,15 +10,43 @@ from pprint import pprint
 from tqdm import tqdm
 from colorama import Fore, Style
 from selenium.webdriver.common.by import By
-
+from termcolor import colored
 
 
 # Prompt the user to input the URL
 url = input('Enter the URL: ')
-domain = urlparse(url).netloc
-print(domain)
-ip=socket.gethostbyname(domain)
-print(socket.gethostbyname(domain))
+
+# Check if the input starts with http:// or https://
+if not url.startswith('http://') and not url.startswith('https://'):
+    url = 'http://' + url
+
+try:
+    domain = urlparse(url).netloc
+    ip = socket.gethostbyname(domain)
+    print(ip)
+except (socket.gaierror, ValueError, TypeError, AttributeError) as e:
+    print(f"Error: {e}")
+target = ip  # Replace with your target IP address
+start_port = 1
+end_port = 100  # Replace with the highest port you want to scan
+
+for port in range(start_port, end_port+1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.1)  # Set a timeout for the connection attempt
+    try:
+        s.connect((target, port))
+    except (socket.timeout, ConnectionRefusedError):
+        pass  # Ignore timeouts and refused connections
+    else:
+        print(f"Port {port} is open")
+    finally:
+        s.close()
+url = "http://example.com"
+data = {"param1": "value1", "param2": "value2"}
+
+response = requests.post(url, data=data)
+server_info = response.headers["Server"]
+print(f"Server information: {server_info}")
 # Make a GET request to the specified URL with the headers
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
@@ -71,24 +99,27 @@ with open('temp02.html', 'w') as f:
 # Open the temporary HTML file in the default web browser
 webbrowser.open('temp02.html')
 
-#test csp
+#test security header
 
+print(Fore.BLUE + Style.BRIGHT+"Test Security header")
 
-print(Fore.GREEN + Style.BRIGHT +"Test CSP")
+security_headers = [
+    "Content-Security-Policy",
+    "X-Content-Type-Options",
+    "X-Frame-Options",
+    "X-XSS-Protection",
+    "Strict-Transport-Security",
+]
 
-# Send an HTTP request to the target web page
+# Make a GET request to the specified URL
 response = requests.get(url)
-print(Fore.BLUE + Style.BRIGHT+"request header")
-pprint(response.request.headers)
-print(Fore.GREEN + Style.BRIGHT +"response header")
-pprint(response.headers)
 
-# Check if the response has a Content-Security-Policy header
-if 'Content-Security-Policy' in response.headers:
-    csp_header = response.headers['Content-Security-Policy']
-    print('CSP Header: ', csp_header)
-else:
-    print('No CSP Header Found')
+# Check if the security headers are present in the response
+for header in security_headers:
+    if header in response.headers:
+        print(f"{header}: {response.headers[header]}")
+    else:
+        print(f"{header} header is missing")
 
 print(Style.RESET_ALL)
 
@@ -134,22 +165,22 @@ print('\n[+] Test Complete!')
 
 
 wordlist_file = input("Enter the path to the wordlist file: ")
+
+# Read directory names from the wordlist file
 with open(wordlist_file, "r", encoding="iso-8859-1") as f:
     directories = [line.strip() for line in f]
 
-# Send a GET request to the website and get the response content
-response = requests.get(url)
-content = response.content.decode("utf-8")
-
-# Find the URLs in the response content that contain the directory names
+# Iterate through each directory name and send a GET request to the URL
 for directory in directories:
-    directory_url = f"{url}/{directory} ||"
+    directory_url = f"{url}/{directory}"
     response = requests.get(directory_url)
-   
-    if response.status_code == 200:
-    	print(directory_url+" || "+str(response))
+    status_code = response.status_code
+    
+    # Print directory URL and response status code
+    if status_code == 200:
+        print(colored(f"{directory_url} || {status_code}", "yellow"))
     else:
-    	print(directory_url+" || "+str(response))
+        print(f"{directory_url} || {status_code}")
     	
 scanner = nmap.PortScanner()
 
